d2d84f5d95c331af50feb0ec8f655c2945d4ee0e
Sylvester <syni0001@stud.kea.dk>
2023-12-11 09:32:10 +0100

    Hej

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 /***** VIDEO *****/
 /* videoElement.play(); */
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerMenu = document.querySelector(".burger-menu");
+  const navMenu = document.querySelector(".nav-menu");
+
+  burgerMenu.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
+    burgerMenu.classList.toggle("active");
+  });
+});
